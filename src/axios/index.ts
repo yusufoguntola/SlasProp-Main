7c674eb4792db008050cf342e46b7856b612697c
Afolabi Monsuru <monsuruafolabi463@gmail.com>
2024-11-21 13:14:44 +0100

    chore: update env

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -3,7 +3,8 @@ import { useAuthStore } from "../stores/useAuthStore";
 
 export const axiosInstance = axios.create({
   baseURL:
-    process.env.REACT_APP_API_BASE_URL || "http://46.101.149.167:5001/api/v1",
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://slas-prop.ganafsmas.com/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
