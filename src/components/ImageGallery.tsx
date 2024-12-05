dd80f5384206d713f3e03028c45db3b385af61d8
Nasirudeen Olawale <gemdajs@gmail.com>
2024-12-05 06:34:00 +0100

    update

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -5,7 +5,7 @@ import ReactImageGallery from "react-image-gallery";
 import { Container } from "@mui/material";
 
 export function ImageGallery(property: Property) {
-  const images = property.images.map((i) => {
+  const images = (property?.images || []).map((i) => {
     return {
       original: i,
       thumbnail: i,
