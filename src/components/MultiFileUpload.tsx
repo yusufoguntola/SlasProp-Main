d3892557840dbaf9bf5060db9b53725f6c5d6dda
Nasirudeen Olawale <gemdajs@gmail.com>
2024-12-05 12:01:40 +0100

    update

--- src/components/MultiFileUpload.tsx
+++ src/components/MultiFileUpload.tsx
@@ -53,93 +53,93 @@ export function MultipleFileUpload({ setImages }: MultipleFileUploadProps) {
   };
 
   return (
-    <Box
-      border="1px dashed #ccc"
-      borderRadius={2}
-      textAlign="center"
-      sx={{
-        ml: 2,
-        width: "100%",
-        backgroundColor: "#f5f5f5",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      {/* Display the uploaded images as thumbnails */}
-      <Grid
-        container
-        gridColumn="1"
-        spacing={2}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        sx={{ pb: 3, mx: 0, background: "black" }}
+    <Box sx={{ flexGrow: 1, ml: 2, width: "100%", pt: 4 }}>
+      <Box
+        border="1px dashed #ccc"
+        borderRadius={2}
+        textAlign="center"
+        sx={{
+          width: "100%",
+          backgroundColor: "#f5f5f5",
+          display: "flex",
+          flexDirection: "column",
+          py: 4,
+        }}
       >
-        {uploadedImages.map((url, index) => (
-          <Grid key={url} sx={{ mx: 0 }}>
-            <Box
-              sx={{
-                position: "relative",
-                borderRadius: 1,
-                overflow: "hidden",
-                px: 0,
-              }}
-            >
-              <img
-                src={url}
-                alt={`Thumbnail ${index}`}
-                style={{
-                  width: "100%",
-                  height: "80px",
-                  objectFit: "cover",
-                  margin: 0,
-                }}
-              />
-              <IconButton
-                onClick={() => handleRemoveImage(url)}
+        {/* Display the uploaded images as thumbnails */}
+        <Grid
+          container
+          gridColumn="1"
+          spacing={2}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          sx={{ mx: 0, mb: 2 }}
+        >
+          {uploadedImages.map((url, index) => (
+            <Grid key={url} sx={{ mx: 0 }}>
+              <Box
                 sx={{
-                  position: "absolute",
-                  top: "4px",
-                  right: "4px",
-                  backgroundColor: "rgba(0, 0, 0, 0.7)",
-                  color: "white",
-                  fontSize: "10px",
-                  "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.9)" },
+                  position: "relative",
+                  borderRadius: 1,
+                  overflow: "hidden",
+                  px: 0,
                 }}
               >
-                <Delete fontSize="small" />
-              </IconButton>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
+                <img
+                  src={url}
+                  alt={`Thumbnail ${index}`}
+                  style={{
+                    width: "150px",
+                    maxHeight: "80px",
+                    objectFit: "cover",
+                    margin: 0,
+                  }}
+                />
+                <IconButton
+                  onClick={() => handleRemoveImage(url)}
+                  sx={{
+                    position: "absolute",
+                    top: "4px",
+                    right: "4px",
+                    backgroundColor: "rgba(0, 0, 0, 0.7)",
+                    color: "white",
+                    fontSize: "10px",
+                    "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.9)" },
+                  }}
+                >
+                  <Delete fontSize="small" />
+                </IconButton>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
 
-      {/* Upload input */}
-      <Box mt={2} display="flex" justifyContent="center">
-        <input
-          type="file"
-          accept="image/*"
-          multiple
-          onChange={handleUpload}
-          style={{ display: "none" }}
-          id="upload-input"
-        />
-        <label htmlFor="upload-input">
-          <Button
-            component="span"
-            variant="contained"
-            sx={{
-              backgroundColor: "#1976d2",
-              color: "white",
-              textTransform: "capitalize",
-              fontSize: "14px",
-              mb: 2,
-              "&:hover": { backgroundColor: "#125aa1" },
-            }}
-          >
-            {loading ? "Uploading..." : "Upload Images"}
-          </Button>
-        </label>
+        {/* Upload input */}
+        <Box display="flex" justifyContent="center">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleUpload}
+            style={{ display: "none" }}
+            id="upload-input"
+          />
+          <label htmlFor="upload-input">
+            <Button
+              component="span"
+              variant="contained"
+              sx={{
+                backgroundColor: "#1976d2",
+                color: "white",
+                textTransform: "capitalize",
+                fontSize: "14px",
+                "&:hover": { backgroundColor: "#125aa1" },
+              }}
+            >
+              {loading ? "Uploading..." : "Upload Images"}
+            </Button>
+          </label>
+        </Box>
       </Box>
     </Box>
   );
