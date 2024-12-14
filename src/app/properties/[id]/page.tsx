22c4573a1f28c11ba574454d5e17c6714761b43d
Hemense Lan <lihemen@gmail.com>
2024-12-14 12:03:17 +0100

    fix build

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -24,7 +24,7 @@ export default function PropertyDetails({
     success: (
       <>
         <NavBarContainer />
-        <ImageGallery {...property} />
+        <ImageGallery property={property} />
         <DetailsBox property={property} />
       </>
     ),
--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -4,7 +4,7 @@ import ReactImageGallery from "react-image-gallery";
 
 import { Container } from "@mui/material";
 
-export function ImageGallery(property: Property) {
+export function ImageGallery({ property }: { property?: Property }) {
   const images = (property?.images || []).map((i) => {
     return {
       original: i,
