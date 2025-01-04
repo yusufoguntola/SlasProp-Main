8e2fc513094d4e55c96e102a91ca0d65ed0cefe1
Hemense Lan <lihemen@gmail.com>
2025-01-04 18:14:00 +0100

    error handler

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -5,6 +5,8 @@ import { showToast } from "./toast";
 export function onError(err: unknown) {
   const error = err as AxiosError<{ error: string; message: string }>;
 
-  console.error(error.response?.data.error);
-  return showToast("error", error.response?.data.error);
+  if ("response" in error) {
+    return showToast("error", `${error.response?.data.error}`);
+  }
+  return showToast("error", error.message);
 }
