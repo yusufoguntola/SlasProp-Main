89b86b7560df36a6712fbecf218db736416bf3a1
Hemense Lan <lihemen@gmail.com>
2025-02-04 14:39:57 +0100

    properties list

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,9 @@
-import { cookies } from "next/headers";
-
-import RegisteredProperties from "@/components/RegisteredProperties";
 import { PropertiesList } from "@/components/properties/list";
 
 export default async function Page() {
-  const cookieStore = await cookies();
-
-  const slider = cookieStore.get("slider");
-
   return (
-    <div key={slider?.toString()} className="w-full">
-      {slider?.value === "true" ? <RegisteredProperties /> : <PropertiesList />}
+    <div className="w-full">
+      <PropertiesList />
     </div>
   );
 }
